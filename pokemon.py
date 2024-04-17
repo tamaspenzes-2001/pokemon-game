9996bf60a089fe2a9dab82729a9a90f93a5745fb
tamaspenzes-2001 <penzestamas@proton.me>
2024-04-17 13:12:27 +0200

    print messages to users

--- pokemon.py
+++ pokemon.py
@@ -9,12 +9,15 @@ class Pokémon:
 
   def knock_out(self):
     self.knocked_out = True
+    print(f"{self.name} is knocked out!")
 
   def revive(self):
     self.knocked_out = False
     self.health = self.max_health
+    print(f"{self.name} is revived!")
 
   def lose_health(self, number):
     self.health -= number
+    print(f"{self.name} lost {number} health, now has {self.health} health.")
     if self.health <= 0:
       self.knock_out()
