5543e5c4e4f4d821a079c5e2f172e926e613ebb5
tamaspenzes-2001 <penzestamas@proton.me>
2024-04-17 13:06:25 +0200

    implement health handling for Pokémons

--- pokemon.py
+++ pokemon.py
@@ -6,3 +6,15 @@ class Pokémon:
     self.max_health = level + 2
     self.health = self.max_health
     self.knocked_out = False
+
+  def knock_out(self):
+    self.knocked_out = True
+
+  def revive(self):
+    self.knocked_out = False
+    self.health = self.max_health
+
+  def lose_health(self, number):
+    self.health -= number
+    if self.health <= 0:
+      self.knock_out()
